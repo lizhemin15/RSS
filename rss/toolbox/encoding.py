ffeb61634c5d6fbd875e966782f57d1df8f9539a
li zhemin <lizhemin15@126.com>
2023-07-10 00:18:20 +0800

    update

--- rss/toolbox/encoding.py
+++ rss/toolbox/encoding.py
@@ -2,7 +2,7 @@ import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
 
-def linear_interp(voxel_embedds=None, weights=None, x=None, voxel_min_vertex=None, voxel_max_vertex=None):
+def linear_interp(x=None, voxel_min_vertex=None, voxel_max_vertex=None,voxel_embedds=None):
     """
     Performs linear interpolation.
 
@@ -20,34 +20,19 @@ def linear_interp(voxel_embedds=None, weights=None, x=None, voxel_min_vertex=Non
     """
 
     # 获取输入张量的维度
-    if x is None:
-        d = weights.shape[1]
-    else:
-        d = x.shape[1]
-
-
+    d = x.shape[1]
     # 递归终止条件，当维度d为1时，直接返回结果
     if d == 1:
-        return x, voxel_min_vertex, voxel_max_vertex, voxel_embedds
+        return voxel_embedds
     
     weights = ((x - voxel_min_vertex)/(voxel_max_vertex-voxel_min_vertex))[:,-1][:,None] # B,
     # voxel_embedds B,2^(d-1),d'
     # index二分加权求和
-    new_voxel_embedds = voxel_embedds[:,:2^(d-1)]*(1-weights) + voxel_embedds[:,2^(d-1):]*weights # B,2^(d-1),d'
+    new_voxel_embedds = voxel_embedds[:,:2**(d-1)]*(1-weights) + voxel_embedds[:,2**(d-1):]*weights # B,2^(d-1),d'
+    new_voxel_min_vertex = voxel_min_vertex[:,:-1] # B x (d-1)
+    new_voxel_max_vertex = voxel_max_vertex[:,:-1] # B x (d-1)
+    new_x = x[:,:-1] # B x (d-1)
 
-    # step 1
-    # 0->000, 1->001, 2->010, 3->011, 4->100, 5->101, 6->110, 7->111
-    c00 = voxel_embedds[:,0]*(1-weights[:,0][:,None]) + voxel_embedds[:,4]*weights[:,0][:,None]
-    c01 = voxel_embedds[:,1]*(1-weights[:,0][:,None]) + voxel_embedds[:,5]*weights[:,0][:,None]
-    c10 = voxel_embedds[:,2]*(1-weights[:,0][:,None]) + voxel_embedds[:,6]*weights[:,0][:,None]
-    c11 = voxel_embedds[:,3]*(1-weights[:,0][:,None]) + voxel_embedds[:,7]*weights[:,0][:,None]
-
-    # step 2
-    c0 = c00*(1-weights[:,1][:,None]) + c10*weights[:,1][:,None]
-    c1 = c01*(1-weights[:,1][:,None]) + c11*weights[:,1][:,None]
-
-    # step 3
-    c = c0*(1-weights[:,2][:,None]) + c1*weights[:,2][:,None]
     return new_x, new_voxel_min_vertex, new_voxel_max_vertex, new_voxel_embedds
     
 
