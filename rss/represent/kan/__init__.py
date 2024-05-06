9127234c1cdbbea4e141e06a5c8dc2fac1097223
jamily <214476062@qq.com>
2024-05-06 14:48:14 +0800

    Update __init__.py

--- rss/represent/kan/__init__.py
+++ rss/represent/kan/__init__.py
@@ -5,10 +5,10 @@ __all__ = ["EFF_KAN"]
 
 
 def get_kan(parameter):
-    de_para_dict = {'dim_in':2,'dim_hidden':100,'dim_out':1,'num_layers':4}
+    de_para_dict = {'dim_in':2,'dim_hidden':100,'dim_out':1,'num_layers':4,'spline_type':'spline'}
     for key in de_para_dict.keys():
         param_now = parameter.get(key,de_para_dict.get(key))
         parameter[key] = param_now
     # print('MLP : ',parameter)
     if parameter.get('net_name','SIREN') == "EFF_KAN":
-        return EFF_KAN([parameter['dim_in'],*[parameter['dim_hidden']]*parameter['num_layers'],parameter['dim_out']])
+        return EFF_KAN([parameter['dim_in'],*[parameter['dim_hidden']]*parameter['num_layers'],parameter['dim_out']],spline_type=parameter['spline_type'])
