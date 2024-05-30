0d80f7a16c36389cbdf13c78ef9b53215c25243a
jamily <214476062@qq.com>
2024-05-31 07:47:09 +0800

    commit_message

--- rss/toolbox/gen_mask.py
+++ rss/toolbox/gen_mask.py
@@ -64,7 +64,7 @@ def load_mask(mask_type='random',random_rate=0.0,mask_path=None,data_shape=None,
         return mask
     elif mask_type == 'extend':
         mask = np.zeros((data_shape[0],data_shape[1]))
-        mask[data_shape[0]//4:data_shape[0]*3//4,data_shape[0]//4:data_shape[0]*3//4] = 1
+        mask[data_shape[0]//8:data_shape[0]*7//8,data_shape[0]//8:data_shape[0]*7//8] = 1
         if len(data_shape) == 3:
             mask = np.expand_dims(mask,axis=2)
         elif len(data_shape) == 4:
