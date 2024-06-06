cf06a0513814e38c367537645cacaab1ff8b4c6e
jamily <214476062@qq.com>
2024-06-06 15:00:21 +0800

    commit_message

--- rss/represent/reg.py
+++ rss/represent/reg.py
@@ -303,6 +303,7 @@ class regularizer(nn.Module):
         else:
             n = M.shape[0]
             k_nearest_values = M[t.tensor(self.indices)]  # 形状为 (n, n, k)
+            print(k_nearest_values.shape)
             k_nearest_distances = np.take_along_axis(self.distances.reshape(n, n, n*n), self.indices, axis=-1)  # 形状为 (n, n, k)
             k_nearest_distances = t.exp(-t.from_numpy(k_nearest_distances).to(dtype=M.dtype, device=M.device))
             return t.mean(k_nearest_distances*(M.unsqueeze(-1)-k_nearest_values)**2)
