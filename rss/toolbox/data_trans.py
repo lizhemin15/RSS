0b29eca6b7a08c69c6335435789b5e5e5ee55705
jamily <lizhemin15@126.com>
2024-07-11 12:22:26 +0800

    commit_message

--- rss/toolbox/data_trans.py
+++ rss/toolbox/data_trans.py
@@ -63,10 +63,14 @@ def downsample_tensor(input_tensor, factor):
 def A2lap(A_0):
     n = A_0.shape[0]
     device = A_0.device
-    Ones = t.ones(n,1)
-    I_n = t.from_numpy(np.eye(n)).to(t.float32)
-    Ones = to_device(Ones,device)
-    I_n = to_device(I_n,device)
-    A_1 = A_0 * (t.mm(Ones,Ones.T)-I_n) # A_1 将中间的元素都归零，作为邻接矩阵
-    L = -A_1+t.mm(A_1,t.mm(Ones,Ones.T))*I_n # A_2 将邻接矩阵转化为拉普拉斯矩阵
-    return L
+    Ones = t.ones(n, 1, device=device)
+    I_n = t.eye(n, device=device, dtype=t.float32)
+    
+    A_1 = A_0 * (t.mm(Ones,Ones.T) - I_n)  # A_1 将中间的元素都归零，作为邻接矩阵
+    D = t.diag(t.sum(A_1, axis=1))  # 度矩阵
+    L = D - A_1  # 拉普拉斯矩阵
+    
+    D_sqrt_inv = t.diag(1.0 / t.sqrt(t.diag(D)))  # 度矩阵的开方逆
+    L_normalized = t.mm(t.mm(D_sqrt_inv, L), D_sqrt_inv)  # 归一化的拉普拉斯矩阵
+    
+    return L_normalized
