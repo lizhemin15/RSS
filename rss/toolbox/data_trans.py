794675dc9c45a1457db2d8308d4ea8d353b488c0
jamily <lizhemin15@126.com>
2024-07-11 10:37:10 +0800

    commit_message

--- rss/toolbox/data_trans.py
+++ rss/toolbox/data_trans.py
@@ -58,3 +58,15 @@ def downsample_tensor(input_tensor, factor):
     output_tensor = input_tensor.mean(dim=(1, 3))
 
     return output_tensor
+
+
+def A2lap(A_0):
+    n = A_0.shape[0]
+    device = A_0.device
+    Ones = t.ones(n,1)
+    I_n = t.from_numpy(np.eye(n)).to(t.float32)
+    Ones = to_device(Ones,device)
+    I_n = to_device(I_n,device)
+    A_1 = A_0 * (t.mm(Ones,Ones.T)-I_n) # A_1 将中间的元素都归零，作为邻接矩阵
+    L = -A_1+t.mm(A_1,t.mm(Ones,Ones.T))*I_n # A_2 将邻接矩阵转化为拉普拉斯矩阵
+    return L
