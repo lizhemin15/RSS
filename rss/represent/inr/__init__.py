e7b2404190e53de41c868815033ca7d7d62b0514
jamily <lizhemin15@126.com>
2024-10-11 11:02:17 +0800

    commit_message

--- rss/represent/inr/__init__.py
+++ rss/represent/inr/__init__.py
@@ -1,6 +1,7 @@
 from rss.represent.inr.mlp import MLP
 from rss.represent.inr.siren import SIREN
 from rss.represent.inr.wire import WIRE
+from rss.represent.inr.bacon import BACONS
 
 
-__all__ = ["MLP","SIREN","WIRE"]
+__all__ = ["MLP","SIREN","WIRE","BACONS"]
