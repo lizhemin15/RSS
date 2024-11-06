d3c5dcba5bbe272c9e1dded41a7f655ea620df0d
jamily <lizhemin15@126.com>
2024-11-06 09:00:40 +0800

    commit_message

--- rss/represent/representer.py
+++ rss/represent/representer.py
@@ -350,7 +350,7 @@ class SIMINER(DINER):
 
     def forward(self, x):
         self.forward_count += 1
-        if self.forward_count % 10 == 0:
+        if self.forward_count % 10 == 0 and self.forward_count < 1000:
             self.update_G()
         if x.dim() == 3:
             x = x.squeeze(0)  # 去掉第一维
