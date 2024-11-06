acfff9a13ec4781b5b19de4403f4aacbc961e574
jamily <lizhemin15@126.com>
2024-11-06 18:54:06 +0800

    commit_message

--- rss/represent/representer.py
+++ rss/represent/representer.py
@@ -253,7 +253,7 @@ class DINER(nn.Module):
         self.G = nn.Parameter(t.randn(G_shape) * 1e-3)
 
         # 神经网络部分
-        inr_para = parameter.get('inr_para', {'net_name': 'SIREN'})
+        inr_para = parameter.get('inr_para', {'net_name': 'MLP'})
         inr_para['dim_out'] = parameter.get('dim_out', 1)
         inr_para['dim_in'] = self.feature_dim
         self.net = get_nn(inr_para)
