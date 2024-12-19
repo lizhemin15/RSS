59d0024dfb2fe037c13ed34c35c99c0551d535fe
jamily <lizhemin15@126.com>
2024-12-19 16:07:10 +0800

    commit_message

--- rss/represent/feature/hash.py
+++ rss/represent/feature/hash.py
@@ -44,28 +44,26 @@ class HashEmbedders(nn.Module):
     def __init__(self, bounding_box=[-1, 1], n_levels=16, n_features_per_level=2,
                  log2_hashmap_size=19, base_resolution=16, finest_resolution=512, non_local_if=False):
         super(HashEmbedders, self).__init__()
-        # ('bounding_box:',bounding_box)
         self.forward_count = 0
         self.non_local_if = non_local_if
         self.bounding_box = bounding_box
         self.n_levels = n_levels
         self.n_features_per_level = n_features_per_level
         self.log2_hashmap_size = log2_hashmap_size
-        self.base_resolution = torch.tensor(base_resolution, device=device)
-        self.finest_resolution = torch.tensor(finest_resolution, device=device)
+        self.base_resolution = torch.tensor(base_resolution)
+        self.finest_resolution = torch.tensor(finest_resolution)
         self.out_dim = self.n_levels * self.n_features_per_level
 
         self.b = torch.exp((torch.log(self.finest_resolution) - torch.log(self.base_resolution)) / (n_levels - 1))
 
         self.embeddings = nn.ModuleList([nn.Embedding(2 ** self.log2_hashmap_size,
-                                                      self.n_features_per_level).to(device) for i in range(n_levels)])
+                                                      self.n_features_per_level) for i in range(n_levels)])
         # custom uniform initialization
         for i in range(n_levels):
             nn.init.uniform_(self.embeddings[i].weight, a=-0.0001, b=0.0001)
 
     def trilinear_interp(self, x, voxel_min_vertex, voxel_max_vertex, voxel_embedds):
         weights = (x - voxel_min_vertex) / (voxel_max_vertex - voxel_min_vertex)  # B x 3
-
         c00 = voxel_embedds[:, 0] * (1 - weights[:, 0][:, None]) + voxel_embedds[:, 4] * weights[:, 0][:, None]
         c01 = voxel_embedds[:, 1] * (1 - weights[:, 0][:, None]) + voxel_embedds[:, 5] * weights[:, 0][:, None]
         c10 = voxel_embedds[:, 2] * (1 - weights[:, 0][:, None]) + voxel_embedds[:, 6] * weights[:, 0][:, None]
@@ -80,13 +78,17 @@ class HashEmbedders(nn.Module):
 
     def bilinear_interp(self, x, voxel_min_vertex, voxel_max_vertex, voxel_embedds):
         weights = (x - voxel_min_vertex) / (voxel_max_vertex - voxel_min_vertex)  # B x 2
-
         c00 = voxel_embedds[:, 0] * (1 - weights[:, 0][:, None]) + voxel_embedds[:, 2] * weights[:, 0][:, None]
         c01 = voxel_embedds[:, 1] * (1 - weights[:, 0][:, None]) + voxel_embedds[:, 3] * weights[:, 0][:, None]
 
         c = c00 * (1 - weights[:, 1][:, None]) + c01 * weights[:, 1][:, None]
 
         return c
+
+    def linear_interp(self, x, voxel_min_vertex, voxel_max_vertex, voxel_embedds):
+        weights = (x - voxel_min_vertex) / (voxel_max_vertex - voxel_min_vertex)  # B x 1
+        c0 = voxel_embedds[:, 0] * (1 - weights) + voxel_embedds[:, 1] * weights
+        return c0
 
     def forward(self, x):
         self.forward_count += 1
@@ -105,7 +107,7 @@ class HashEmbedders(nn.Module):
                     x_embedded_all.append(x_embedded)
 
                 keep_mask = keep_mask.sum(dim=-1) == keep_mask.shape[-1]
-                return torch.cat(x_embedded_all, dim=-1)#, keep_mask
+                return torch.cat(x_embedded_all, dim=-1)
 
             elif x.shape[1] == 2:
                 x_embedded_all = []
@@ -121,7 +123,23 @@ class HashEmbedders(nn.Module):
                     x_embedded_all.append(x_embedded)
 
                 keep_mask = keep_mask.sum(dim=-1) == keep_mask.shape[-1]
-                return torch.cat(x_embedded_all, dim=-1)#, keep_mask
+                return torch.cat(x_embedded_all, dim=-1)
+
+            elif x.shape[1] == 1:
+                x_embedded_all = []
+                for i in range(self.n_levels):
+                    resolution = torch.floor(self.base_resolution * self.b ** i).to(x.device)
+                    voxel_min_vertex, voxel_max_vertex, hashed_voxel_indices, keep_mask = get_voxel_vertices(
+                        x, self.bounding_box,
+                        resolution, self.log2_hashmap_size, is_3d=False)
+
+                    voxel_embedds = self.embeddings[i](hashed_voxel_indices)
+
+                    x_embedded = self.linear_interp(x, voxel_min_vertex, voxel_max_vertex, voxel_embedds)
+                    x_embedded_all.append(x_embedded)
+
+                keep_mask = keep_mask.sum(dim=-1) == keep_mask.shape[-1]
+                return torch.cat(x_embedded_all, dim=-1)
 
             else:
                 raise ValueError("Unsupported input dimension: {}".format(x.shape[1]))
@@ -129,6 +147,8 @@ class HashEmbedders(nn.Module):
             if x.shape[1] == 3:
                 pass
             elif x.shape[1] == 2:
+                pass
+            elif x.shape[1] == 1:
                 pass
             else:
                 raise ValueError("Unsupported input dimension: {}".format(x.shape[1]))
