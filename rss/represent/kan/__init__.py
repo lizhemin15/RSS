f0abbfa4c2fd81574df4b3d3ce0767a79f65717e
jamily <lizhemin15@126.com>
2025-04-11 09:18:11 +0800

    commit_message

--- rss/represent/kan/__init__.py
+++ rss/represent/kan/__init__.py
@@ -12,11 +12,11 @@ def get_kan(parameter):
         parameter[key] = param_now
     # print('KAN : ',parameter)
     if parameter.get('net_name','SIREN') == "EFF_KAN":
-        return EFF_KAN([parameter['dim_in'],*[parameter['dim_hidden']]*parameter['num_layers'],
-                      parameter['dim_out']],spline_type=parameter['spline_type'],grid_size=parameter['grid_size'],
+        return EFF_KAN([parameter['dim_in'],*[parameter['dim_hidden']]*parameter['num_layers'],parameter['dim_out']],
+                       spline_type=parameter['spline_type'],grid_size=parameter['grid_size'],
                       layer_norm = parameter['layer_norm'])
     elif parameter.get('net_name','SIREN') == "FastKAN":
-        return FastKAN(layers_hidden=[parameter['dim_in'],*[parameter['dim_hidden']]*parameter['num_layers']],
+        return FastKAN(layers_hidden=[parameter['dim_in'],*[parameter['dim_hidden']]*parameter['num_layers'],parameter['dim_out']],
                        grid_min=-1,grid_max=1,
                        num_grids=parameter['grid_size'])
     else:
