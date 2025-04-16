72fce43a9177ee99c9baa66582298e14481c76d1
jamily <lizhemin15@126.com>
2025-04-16 11:03:50 +0800

    commit_message

--- rss/represent/inr/__init__.py
+++ rss/represent/inr/__init__.py
@@ -5,16 +5,9 @@ from rss.represent.inr.bacon import BACONS
 from rss.represent.inr.mfn import GaborNets,FourierNets
 from rss.represent.inr.gauss import GAUSS
 from rss.represent.inr.finer import FINER
-from rss.represent.inr.afn import AFN
-from rss.represent.inr.eoa import EOA
-from rss.represent.inr.apw import APW
-from rss.represent.inr.pps import PPS
-from rss.represent.inr.fqps import FQPS
-from rss.represent.inr.ffps import FFPS
-from rss.represent.inr.ffcps import FFCPS
-from torch import nn
 
-__all__ = ["MLP","SIREN","WIRE","BACONS","GaborNets","FourierNets","GAUSS","FINER", "AFN", "EOA", "APW", "PPS", "FQPS", "FFPS", "FFCPS"]
+
+__all__ = ["MLP","SIREN","WIRE","BACONS","GaborNets","FourierNets","GAUSS","FINER"]
 
 
 
--- rss/represent/representer.py
+++ rss/represent/representer.py
@@ -7,7 +7,7 @@ from scipy.ndimage import gaussian_filter
 import numpy as np
 
 
-from rss.represent.inr import MLP,SIREN,WIRE,BACONS,FourierNets,GaborNets,AFN,EOA,PPS,FQPS,FFPS,FFCPS
+from rss.represent.inr import MLP,SIREN,WIRE,BACONS,FourierNets,GaborNets
 from rss.represent.tensor import DMF,TF
 from rss.represent.utils import reshape2
 from rss.represent.interpolation import Interpolation
@@ -74,18 +74,6 @@ def get_nn(parameter={}):
         net = GAUSS(parameter)
     elif net_name == 'FINER':
         net = FINER(parameter)
-    elif net_name == 'AFN':
-        net = AFN(parameter)
-    elif net_name == 'EOA':
-        net = EOA(parameter)
-    elif net_name == 'PPS':
-        net = PPS(parameter)
-    elif net_name == 'FQPS':
-        net = FQPS(parameter)
-    elif net_name == 'FFPS':
-        net = FFPS(parameter)
-    elif net_name == 'FFCPS':
-        net = FFCPS(parameter)
     else:
         raise ValueError(f'Wrong net_name = {net_name}')
     if clip_if==False:
